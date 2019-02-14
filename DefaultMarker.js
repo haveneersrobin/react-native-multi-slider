b39fdbaea6d88ff5d3a4a6348827d558f6622d29
Robin Haveneers <haveneersrobin@gmail.com>
2019-02-14 19:19:17 +0100

    Added ticks and fixed min distance between two sliders

--- DefaultMarker.js
+++ DefaultMarker.js
@@ -1,22 +1,50 @@
-import React from 'react';
-import { View, StyleSheet, Platform, TouchableHighlight } from 'react-native';
+import React from "react";
+import PropTypes from "prop-types";
 
-class DefaultMarker extends React.Component {
+import {
+  View,
+  StyleSheet,
+  Platform,
+  TouchableHighlight,
+  Text
+} from "react-native";
+
+const ViewPropTypes = require("react-native").ViewPropTypes || View.propTypes;
+
+export default class DefaultMarker extends React.Component {
+  static propTypes = {
+    pressed: PropTypes.bool,
+    pressedMarkerStyle: ViewPropTypes.style,
+    markerStyle: ViewPropTypes.style,
+    enabled: PropTypes.bool,
+    currentValue: PropTypes.number,
+    valuePrefix: PropTypes.string,
+    valueSuffix: PropTypes.string
+  };
+
   render() {
     return (
       <TouchableHighlight>
         <View
-          style={
-            this.props.enabled
-              ? [
-                  styles.markerStyle,
-                  this.props.markerStyle,
-                  this.props.pressed && styles.pressedMarkerStyle,
-                  this.props.pressed && this.props.pressedMarkerStyle,
-                ]
-              : [styles.markerStyle, styles.disabled]
-          }
-        />
+          style={{
+            flexDirection: "column",
+            alignItems: "center"
+          }}
+        >
+          <View
+            style={
+              this.props.enabled
+                ? [
+                    styles.markerStyle,
+                    this.props.markerStyle,
+                    this.props.pressed && styles.pressedMarkerStyle,
+                    this.props.pressed && this.props.pressedMarkerStyle
+                  ]
+                : [styles.markerStyle, styles.disabled]
+            }
+          />
+          <Text>{this.props.currentValue}</Text>
+        </View>
       </TouchableHighlight>
     );
   }
@@ -30,23 +58,23 @@ const styles = StyleSheet.create({
         width: 30,
         borderRadius: 30,
         borderWidth: 1,
-        borderColor: '#DDDDDD',
-        backgroundColor: '#FFFFFF',
-        shadowColor: '#000000',
+        borderColor: "#DDDDDD",
+        backgroundColor: "#FFFFFF",
+        shadowColor: "#000000",
         shadowOffset: {
           width: 0,
-          height: 3,
+          height: 3
         },
         shadowRadius: 1,
-        shadowOpacity: 0.2,
+        shadowOpacity: 0.2
       },
       android: {
         height: 12,
         width: 12,
         borderRadius: 12,
-        backgroundColor: '#0D8675',
-      },
-    }),
+        backgroundColor: "#0D8675"
+      }
+    })
   },
   pressedMarkerStyle: {
     ...Platform.select({
@@ -54,13 +82,11 @@ const styles = StyleSheet.create({
       android: {
         height: 20,
         width: 20,
-        borderRadius: 20,
-      },
-    }),
+        borderRadius: 20
+      }
+    })
   },
   disabled: {
-    backgroundColor: '#d3d3d3',
-  },
+    backgroundColor: "#d3d3d3"
+  }
 });
-
-export default DefaultMarker;
